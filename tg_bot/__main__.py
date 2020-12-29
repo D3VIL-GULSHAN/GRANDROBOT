34d34490269a63e42a8abc2011424af0ff929669
mayurirobot <76698721+mayurirobot@users.noreply.github.com>
2020-12-29 12:00:46 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -145,8 +145,8 @@ def start(bot: Bot, update: Update, args: List[str]):
                                                                        url="t.me/NatthanDrake")],
                                                                                    [InlineKeyboardButton(text="ADD MAYURI SHIINA TO YOUR GROUP",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
-                                                                                   InlineKeyboardButton(text="Source Code",
-                                                                       url="https://github.com/legendx22/GRANDROBOT")
+                                                                                   InlineKeyboardButton(text="🌐Source Code🌐",
+                                                                       url="https://github.com/mayurirobot/GRANDROBOT")
                                                                                  ]]))
 
     else:
