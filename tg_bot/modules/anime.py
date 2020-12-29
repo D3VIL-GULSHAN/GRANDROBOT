c61e508228980cc3e4062d7304ff7653c122b101
mayurirobot <76698721+mayurirobot@users.noreply.github.com>
2020-12-29 18:18:27 +0530

    Update anime.py

--- tg_bot/modules/anime.py
+++ tg_bot/modules/anime.py
@@ -5,8 +5,8 @@ import textwrap
 import bs4
 import jikanpy
 import requests
-from mayurirobot import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
-from mayurirobot.modules.disable import DisableAbleCommandHandler
+from GRANDROBOT import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
+from GRANDROBOT.modules.disable import DisableAbleCommandHandler
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
                       Update)
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
