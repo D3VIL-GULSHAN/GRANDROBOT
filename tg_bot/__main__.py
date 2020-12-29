78a3828cd20da16e9cc0bcb99dcff49ac2a9bfdd
rampagespike <76698721+rampagespike@users.noreply.github.com>
2020-12-29 10:45:39 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -146,7 +146,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                                                                                    [InlineKeyboardButton(text="ADD MAYURI SHIINA TO YOUR GROUP",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
                                                                                    InlineKeyboardButton(text="Source Code",
-                                                                       url="https://github.com/NatthanDrake/GRANDROBOT")
+                                                                       url="https://github.com/Mayurirobot/GRANDROBOT")
                                                                                  ]]))
 
     else:
